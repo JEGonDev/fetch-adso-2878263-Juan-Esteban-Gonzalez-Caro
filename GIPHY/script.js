e0b464d6694b3a71266cbb07a09e4a178bf0fa2a
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-09-06 12:31:14 -0500

    Se agrego al script la funcion para obtener la api y otro para obtener los gifs , esta funcionando correctamente falta solo hacerle modificaciones en el CSS

--- GIPHY/script.js
+++ GIPHY/script.js
@@ -1,17 +1,17 @@
 const api_key = 'O37PRGT6Km25LvvcA127s2tFGEwvdcFm'
-let limit = 20
-const urlGifs = `api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=${limit}`
-const containerCards = document.getElementById('cardsContainer')
+let limit = 35
+const urlGifs = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=${limit}`
+const containerGifs = document.getElementById('gifsContainer')
 
 const getAPI = async (URL) => {
     const response = await fetch(URL)
-    const data = response.json()
-    data.data.images.original
+    const data = await response.json()
+    return data.data
 }
 
-const createGifs = (gif) => {
+const createGif = (gif) => {
     const card = document.createElement('div')
-    card.classList.add('card-gif')
+    card.classList.add('gif')
 
     const imgGif = document.createElement('img')
     imgGif.src = gif.images.original.url
@@ -19,5 +19,12 @@ const createGifs = (gif) => {
 
     card.appendChild(imgGif)
 
-    containerCards.appendChild(card)
+    containerGifs.appendChild(card)
 }
+
+const getGif = async () => {
+    const data = await getAPI(urlGifs)
+    data.forEach(gif => createGif(gif));
+}
+
+window.addEventListener('DOMContentLoaded', getGif)
